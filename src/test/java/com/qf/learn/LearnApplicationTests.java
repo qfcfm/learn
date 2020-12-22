df8418c709cfde81ac5a18a2aaf9ebcadba2e829
cfm <419081990@qq.com>
2020-12-22 10:59:02 +0800

    1

--- src/test/java/com/qf/learn/LearnApplicationTests.java
+++ src/test/java/com/qf/learn/LearnApplicationTests.java
@@ -3,7 +3,7 @@ package com.qf.learn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LearnApplicationTests {
 
 	@Test
